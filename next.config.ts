621a788cfc98ede61f8124e2c555d0e81ce15a3b
oasisinjuly <oasisinjuly@gmail.com>
2026-08-25 16:35:15 +0900

    config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export', // static HTML export 설정
+  images: {
+    unoptimized: true, // GitHub Pages 호스팅을 위한 이미지 최적화 비활성화
+  },
 };
 
 export default nextConfig;
